c9c0adb26b47817c5343a94fdd70e5085e311911
eww125 <ewappel@gmail.com>
2015-05-05 11:47:16 -0700

    python pep8 indentation

--- project.py
+++ project.py
@@ -43,148 +43,147 @@ def showLogin():
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
+    print 'received state of %s' %request.args.get('state')
+    print 'login_sesion["state"] = %s' %login_session['state']
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
-  print 'received state of %s' %request.args.get('state')
-  print 'login_sesion["state"] = %s' %login_session['state']
-  if request.args.get('state') != login_session['state']:
-    response = make_response(json.dumps('Invalid state parameter.'), 401)
-    response.headers['Content-Type'] = 'application/json'
-    return response
+    gplus_id = request.args.get('gplus_id')
+    print "request.args.get('gplus_id') = %s" %request.args.get('gplus_id')
+    code = request.data
+    print "received code of %s " % code
 
-  gplus_id = request.args.get('gplus_id')
-  print "request.args.get('gplus_id') = %s" %request.args.get('gplus_id')
-  code = request.data
-  print "received code of %s " % code
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
-  try:
-    # Upgrade the authorization code into a credentials object
-    oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
-    oauth_flow.redirect_uri = 'postmessage'
-    credentials = oauth_flow.step2_exchange(code)
-  except FlowExchangeError:
-    response = make_response(json.dumps('Failed to upgrade the authorization code.'), 401)
-    response.headers['Content-Type'] = 'application/json'
-    return response
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
 
-  # Check that the access token is valid.
-  access_token = credentials.access_token
-  url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
-  h = httplib2.Http()
-  result = json.loads(h.request(url, 'GET')[1])
-
-  # If there was an error in the access token info, abort.
-  if result.get('error') is not None:
-    response = make_response(json.dumps(result.get('error')), 500)
-    response.headers['Content-Type'] = 'application/json'
-
-
-  # Verify that the access token is used for the intended user.
-  gplus_id = credentials.id_token['sub']
-  if result['user_id'] != gplus_id:
-    response = make_response(
-        json.dumps("Token's user ID doesn't match given user ID."), 401)
-    response.headers['Content-Type'] = 'application/json'
-    return response
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
 
 
-  # Verify that the access token is valid for this app.
-  if result['issued_to'] != CLIENT_ID:
-    response = make_response(
-        json.dumps("Token's client ID does not match app's."), 401)
-    print "Token's client ID does not match app's."
-    response.headers['Content-Type'] = 'application/json'
-    return response
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
 
-  stored_credentials = login_session.get('credentials')
-  stored_gplus_id = login_session.get('gplus_id')
-  if stored_credentials is not None and gplus_id == stored_gplus_id:
-    response = make_response(json.dumps('Current user is already connected.'),
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+
+    stored_credentials = login_session.get('credentials')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_credentials is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
                              200)
-    response.headers['Content-Type'] = 'application/json'
+        response.headers['Content-Type'] = 'application/json'
 
-  # Store the access token in the session for later use.
-  login_session['provider'] = 'google'
-  login_session['credentials'] = credentials
-  login_session['gplus_id'] = gplus_id
-  response = make_response(json.dumps('Successfully connected user.', 200))
+    # Store the access token in the session for later use.
+    login_session['provider'] = 'google'
+    login_session['credentials'] = credentials
+    login_session['gplus_id'] = gplus_id
+    response = make_response(json.dumps('Successfully connected user.', 200))
 
-  print "#Get user info"
-  userinfo_url =  "https://www.googleapis.com/oauth2/v1/userinfo"
-  params = {'access_token': credentials.access_token, 'alt':'json'}
-  answer = requests.get(userinfo_url, params=params)
-  data = json.loads(answer.text)
-
-
-  login_session['credentials'] = credentials
-  login_session['gplus_id'] = gplus_id
-  login_session['username'] = data["name"]
-  login_session['picture'] = data["picture"]
-  login_session['email'] = data["email"]
-  print login_session['email']
-
-  # see if user exists, if it doesn't make a new one
-  user_id = getUserID(data["email"])
-  if not user_id:
-    user_id = createUser(login_session)
-  login_session['user_id'] = user_id
+    print "#Get user info"
+    userinfo_url =  "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt':'json'}
+    answer = requests.get(userinfo_url, params=params)
+    data = json.loads(answer.text)
 
 
-  output = ''
-  output +='<h1>Welcome, '
-  output += login_session['username']
-  output += '!</h1>'
-  output += '<img src="'
-  output += login_session['picture']
-  # dimensions of the picture at login:
-  output +=' " style = "width: 300px; height: 300px;border-radius: 50px;-webkit-border-radius: 150px;-moz-border-radius: 50px;"> '
-  flash("you are now logged in as %s"%login_session['username'])
-  return output
+    login_session['credentials'] = credentials
+    login_session['gplus_id'] = gplus_id
+    login_session['username'] = data["name"]
+    login_session['picture'] = data["picture"]
+    login_session['email'] = data["email"]
+    print login_session['email']
+
+    # see if user exists, if it doesn't make a new one
+    user_id = getUserID(data["email"])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+
+
+    output = ''
+    output +='<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    # dimensions of the picture at login:
+    output +=' " style = "width: 300px; height: 300px;border-radius: 50px;-webkit-border-radius: 150px;-moz-border-radius: 50px;"> '
+    flash("you are now logged in as %s"%login_session['username'])
+    return output
 
 
 #DISCONNECT - Revoke a current user's token and reset their login_session
 @app.route("/gdisconnect")
 def gdisconnect():
-  # Only disconnect a connected user.
-  credentials = login_session.get('credentials')
-  if credentials is None:
-    response = make_response(json.dumps('Current user not connected.'),401)
-    response.headers['Content-Type'] = 'application/json'
-    return response
-  access_token = credentials.access_token
-  url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' %access_token
-  h = httplib2.Http()
-  result = h.request(url, 'GET')[0]
+    # Only disconnect a connected user.
+    credentials = login_session.get('credentials')
+    if credentials is None:
+        response = make_response(json.dumps('Current user not connected.'),401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    access_token = credentials.access_token
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' %access_token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
 
-  if result['status'] == '200':
-    # Reset the user's session.
-    del login_session['credentials']
-    del login_session['gplus_id']
-    del login_session['username']
-    del login_session['email']
-    del login_session['picture']
+    if result['status'] == '200':
+        # Reset the user's session.
+        del login_session['credentials']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
 
-    response = make_response(json.dumps('Successfully disconnected.'),200)
-    response.headers['Content-Type'] = 'application/json'
-    return response
-  else:
-    # For whatever reason, the given token was invalid.
-    response = make_response(json.dumps('Failed to revoke token for given user.'),400)
-    response.headers['Content-Type'] = 'application/json'
-    return response
+        response = make_response(json.dumps('Successfully disconnected.'),200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        # For whatever reason, the given token was invalid.
+        response = make_response(json.dumps('Failed to revoke token for given user.'),400)
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
 
 @app.route("/login_test")
 def login_test():
-  credentials = login_session.get('credentials')
-  if credentials is None:
-    response = make_response(json.dumps('Not logged in.'),200)
-    response.headers['Content-Type'] = 'application/json'
-    return response
-  else:
-    response = make_response(json.dumps('Logged in as ' + login_session['username']),200)
-    response.headers['Content-Type'] = 'application/json'
-    return response
+    credentials = login_session.get('credentials')
+    if credentials is None:
+        response = make_response(json.dumps('Not logged in.'),200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        response = make_response(json.dumps('Logged in as ' + login_session['username']),200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
 
 #JSON APIs to view Food Truck Information
@@ -209,26 +208,26 @@ def food_trucksJSON():
 @app.route('/')
 @app.route('/food_truck/')
 def showFoodTrucks():
-  food_trucks = session.query(FoodTruck).order_by(asc(FoodTruck.name))
-  return render_template('food_trucks.html', food_trucks = food_trucks)
+    food_trucks = session.query(FoodTruck).order_by(asc(FoodTruck.name))
+    return render_template('food_trucks.html', food_trucks = food_trucks)
 
 #Create a new food truck
 @app.route('/food_truck/new/', methods=['GET','POST'])
 def newFoodTruck():
-  credentials = login_session.get('credentials')
-  if credentials is None:
-      flash('You must login to create a food truck')
-      return redirect(url_for('showFoodTrucks'))
-  else:
-      if request.method == 'POST':
-          db_query = getUserID(login_session['email'])
-          newFoodTruck = FoodTruck(name = request.form['name'], user_id_database=db_query)
-          session.add(newFoodTruck)
-          flash('New Food Truck %s Successfully Created' % newFoodTruck.name)
-          session.commit()
-          return redirect(url_for('showFoodTrucks'))
-      else:
-          return render_template('newFoodTruck.html')
+    credentials = login_session.get('credentials')
+    if credentials is None:
+        flash('You must login to create a food truck')
+        return redirect(url_for('showFoodTrucks'))
+    else:
+        if request.method == 'POST':
+            db_query = getUserID(login_session['email'])
+            newFoodTruck = FoodTruck(name = request.form['name'], user_id_database=db_query)
+            session.add(newFoodTruck)
+            flash('New Food Truck %s Successfully Created' % newFoodTruck.name)
+            session.commit()
+            return redirect(url_for('showFoodTrucks'))
+        else:
+            return render_template('newFoodTruck.html')
 
 #Edit a food Truck
 @app.route('/food_truck/<int:food_truck_id>/edit/', methods = ['GET', 'POST'])
@@ -246,14 +245,14 @@ def editFoodTruck(food_truck_id):
 #Delete a food_truck
 @app.route('/food_truck/<int:food_truck_id>/delete/', methods = ['GET','POST'])
 def deleteFoodTruck(food_truck_id):
-  food_truckToDelete = session.query(FoodTruck).filter_by(id = food_truck_id).one()
-  if request.method == 'POST':
-    session.delete(food_truckToDelete)
-    flash('%s Successfully Deleted' % food_truckToDelete.name)
-    session.commit()
-    return redirect(url_for('showFoodTrucks', food_truck_id = food_truck_id))
-  else:
-    return render_template('deleteFoodTruck.html',food_truck = food_truckToDelete)
+    food_truckToDelete = session.query(FoodTruck).filter_by(id = food_truck_id).one()
+    if request.method == 'POST':
+        session.delete(food_truckToDelete)
+        flash('%s Successfully Deleted' % food_truckToDelete.name)
+        session.commit()
+        return redirect(url_for('showFoodTrucks', food_truck_id = food_truck_id))
+    else:
+        return render_template('deleteFoodTruck.html',food_truck = food_truckToDelete)
 
 #Show a food_truck menu
 @app.route('/food_truck/<int:food_truck_id>/')
@@ -268,20 +267,19 @@ def showMenu(food_truck_id):
 #Create a new menu item
 @app.route('/food_truck/<int:food_truck_id>/menu/new/',methods=['GET','POST'])
 def newMenuItem(food_truck_id):
-  food_truck = session.query(FoodTruck).filter_by(id = food_truck_id).one()
-  if request.method == 'POST':
-      newItem = MenuItem(name = request.form['name'], description = request.form['description'], price = request.form['price'], course = request.form['course'], food_truck_id = food_truck_id)
-      session.add(newItem)
-      session.commit()
-      flash('New Menu %s Item Successfully Created' % (newItem.name))
-      return redirect(url_for('showMenu', food_truck_id = food_truck_id))
-  else:
-      return render_template('newmenuitem.html', food_truck_id = food_truck_id)
+    food_truck = session.query(FoodTruck).filter_by(id = food_truck_id).one()
+    if request.method == 'POST':
+        newItem = MenuItem(name = request.form['name'], description = request.form['description'], price = request.form['price'], course = request.form['course'], food_truck_id = food_truck_id)
+        session.add(newItem)
+        session.commit()
+        flash('New Menu %s Item Successfully Created' % (newItem.name))
+        return redirect(url_for('showMenu', food_truck_id = food_truck_id))
+    else:
+        return render_template('newmenuitem.html', food_truck_id = food_truck_id)
 
 #Edit a menu item
 @app.route('/food_truck/<int:food_truck_id>/menu/<int:menu_id>/edit', methods=['GET','POST'])
 def editMenuItem(food_truck_id, menu_id):
-
     editedItem = session.query(MenuItem).filter_by(id = menu_id).one()
     food_truck = session.query(FoodTruck).filter_by(id = food_truck_id).one()
     if request.method == 'POST':
@@ -339,7 +337,7 @@ def createUser(login_session):
 
 
 if __name__ == '__main__':
-  app.secret_key = 'super_secret_key'
-  app.debug = True
-  app.run(host = '0.0.0.0', port = 5000)
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 5000)
 # item_catalog
